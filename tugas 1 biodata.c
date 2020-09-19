66404e3aaf29447b0150181d67340b12d515c34e
zulkarnaenfhr <fahrizulkarnaen1@gmail.com>
2020-09-19 14:58:20 +0700

    pertama

---
+++ tugas 1 biodata.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main (){
+	
+		sleep(1);
+		printf("\n");
+		printf("\n\t Nama			: fahri izzuddin zulkarnaen ");
+		sleep(1);
+		printf("\n\t Npm			: 19081010046");
+		sleep (1);
+		printf("\n\t Jenis Kelamin		: Laki - Laki");
+		sleep(1);
+		printf("\n\t Tempat, Tanggal lahir 	: Surabaya, 10 April 2001");
+		sleep (1);
+		printf("\n\t Alamat			: Park Royal Regency Q2-27, buduran, sidoarjo");
+		sleep(1);
+		printf("\n\t Email			: fahrizulkarnaen1@gmail.com");
+		sleep (1);
+		printf("\n\t Agama 			: Islam");
+		sleep(1);	
+		printf("\n\t Kewarganegaraan	: Indonesia");
+		sleep (1);
+		printf("\n\t Status			: Mahasiswa");
+		sleep(1);
+		printf("\n\t Hobi 			: Bermain Skateboard");
+		sleep (1);
+		printf("\n\t Motto Hidup		: Jangan lupa shalat karena yang bisa menyelamatkanmu hanya Allah SWT");
+		sleep(1);
+}
+
